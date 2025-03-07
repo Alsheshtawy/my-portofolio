b4bd0ee3ec244d6986260d4a7945c5f99254d112
Mohamed <99219712+Alsheshtawy@users.noreply.github.com>
2025-03-07 04:37:59 +0200

    Delete src directory

--- src/database/dbConnection.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const dbConnection = mongoose.connect("mongodb://localhost:27017/mygold").then(() => {
-    console.log("Database connected successfully")
-}).catch(() => {
-    console.log("Error connecting to the database");
-})
