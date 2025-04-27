c49dfa9042465b837c7ff2f298cc203299b9b176
Mohamed <99219712+Alsheshtawy@users.noreply.github.com>
2025-04-27 15:25:38 +0300

    Delete src directory

--- src/database/dbConnection.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-
-export const dbConnection = mongoose.connect("mongodb://127.0.0.1:27017/MyGOLD").then(() => {
-    console.log("Database connected successfully")
-}).catch(() => {
-    console.log("Error connecting to the database");
-})
--- src/middlewares/catchError.js
+++
@@ -1,5 +0,0 @@
-export function handelError(fn) {
-    return (req, res, next) => {
-        fn(req, res, next).catch((err) => console.log(err))
-    }
-}
